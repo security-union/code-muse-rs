764a05d358210d354bdb7a513ddc1acf9227a3c8
Griffin Obeid <griffobeid@gmail.com>
2023-04-30 08:02:11 -0400

    Process output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+// TODO: Check OS
+use anyhow::bail;
+use async_openai::{
+    types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
+    Client,
+};
 use clap::Parser;
-use async_openai::{Client, types::{CreateChatCompletionRequestArgs, ChatCompletionRequestMessageArgs, Role}};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    io::Write,
+    process::{Child, Command, Stdio},
+};
+use text_io::read;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -9,15 +21,32 @@ struct Args {
     description: String,
 
     /// Programming language to generate this project in
-    #[arg(short, long, default_value="rust")]
+    #[arg(short, long, default_value = "rust")]
     language: String,
+
+    /// The name of the project which will be the name of the directory created
+    #[arg(short, long, default_value = "myapp")]
+    name: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct OutputJson {
+    steps: Vec<String>,
+    files: HashMap<String, String>,
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let prompt = format!(
-            "Take the following programming language and application requirements and respond with the bash steps to create the project and the files that will fulfill the application requirements.
+            "Take the following programming language, application requirements, and project name then generate two things.
+            1. The steps to create the project in valid bash
+            2. The files that will complete the application requirements that will actually compile and work
+
+            The output must match the provided output json schema and be a valid json.
+
+            Project Name:
+            {name}
 
             Programming Language:
             {language}
@@ -34,6 +63,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }}
 
             Respond ONLY with the data portion of a valid Json object. No schema definition required. No other words.",
+            name=args.name,
             language=args.language,
             description=args.description
     );
@@ -46,13 +76,68 @@ The return Json can include an array of objects as defined by the output schema.
 You are not allowed to return anything but a valid Json object.").build()?,
         ChatCompletionRequestMessageArgs::default().role(Role::User).content(prompt).build()?
     ]).build()?;
+    println!("Sending prompt to OpenAI, please wait... 🤖");
     let res = client.chat().create(req).await?;
-    println!("\nResponse:\n");
-    for choice in res.choices {
-        println!(
-            "{}: Role: {}  Content: {}",
-            choice.index, choice.message.role, choice.message.content
-        );
+    println!("Got a response ✅ Attempting to decode the contents...");
+    println!("Response:\n{}", &res.choices[0].message.content);
+    let contents: OutputJson = serde_json::from_str(&res.choices[0].message.content)?;
+    println!("Success, the robot has obeyed our orders.\n");
+
+    println!("Let's setup the project");
+
+    for step in contents.steps.iter() {
+        println!("Does this step look correct? `{step}`");
+        println!("Please input Y/N");
+        let response: String = read!("{}\n");
+        let cmd = {
+            if response.replace(" ", "").to_uppercase() == "Y" {
+                step.clone()
+            } else {
+                println!("Damn robot, can you fix the command? Otherwise enter `skip` to go to the next step");
+                let response: String = read!("{}\n");
+                if response.contains("skip") {
+                    continue;
+                } else {
+                    response
+                }
+            }
+        };
+
+        let mut child = Command::new("bash")
+            .arg("-c")
+            .arg(&cmd)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+        let status = child.wait()?;
+        if !status.success() {
+            bail!(
+                "Failed to execute `{}`. Exit code: {:?}",
+                cmd,
+                status.code()
+            );
+        }
+    }
+
+    let project_root = std::path::Path::new(&args.name);
+    if !project_root.exists() {
+        println!();
+    }
+
+    for (file_path, file_contents) in contents.files {
+        let path_str = format!("{}/{}", args.name, file_path);
+        let path = std::path::Path::new(&path_str);
+        if path.exists() {
+            let mut file = std::fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .open(path)?;
+            file.write_all(file_contents.as_bytes())?;
+            file.flush()?;
+        } else {
+            println!("Unable to determine exactly where to put files. Please create this project file:\nFile path: {:?}\nFile Contents:\n{}", path, file_contents);
+        }
     }
 
     Ok(())
